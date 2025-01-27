63394d05590a393e110c7a66cc59c3f335455291
Artemy Fillman <mainfillman@gmail.com>
2025-01-27 16:17:28 +0300

    попробовал привязать домен

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+// Импорты маршрутов
 const filmsRoutes = require("./routes/films");
 const blockmainRoutes = require("./routes/blockmain");
 const cartoonsRoutes = require("./routes/cartoons");
 const serialsRoutes = require("./routes/serials");
 
 const app = express();
-const PORT = 5100;
 
+// Настройка middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -23,7 +27,14 @@ app.get("/", (req, res) => {
   res.send("Сервер работает! Используй /api/films, /api/cartoons, /api/serials, или /api/blockmain для получения данных.");
 });
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+// Настройка SSL-сертификатов
+const sslOptions = {
+  key: fs.readFileSync("/etc/ssl/privkey.pem"),  // Путь к вашему приватному ключу
+  cert: fs.readFileSync("/etc/ssl/cert.pem"),   // Путь к вашему SSL-сертификату
+  ca: fs.readFileSync("/etc/ssl/chain.pem")     // Путь к цепочке сертификатов
+};
+
+// Запуск HTTPS-сервера
+https.createServer(sslOptions, app).listen(443, () => {
+  console.log(`Сервер запущен на https://servernextfilms.hub-net.org`);
 });
