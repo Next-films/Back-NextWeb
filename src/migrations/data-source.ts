60038823e5dfe792a8ac79dae10d98cd5a2656fe
ArtemyFillman <mainfillman@gmail.com>
2026-04-14 12:53:11 +0300

    исправлены ошибки

--- src/migrations/data-source.ts
+++ src/migrations/data-source.ts
@@ -1,16 +1,23 @@
 import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
 import { EnvironmentVariable } from '@/settings/configuration';
 import { DatabaseSettings } from '../settings/settings-type/database.settings';
-dotenv.config({ path: './.env.dev' });
 
 const dbSettings = new DatabaseSettings(process.env as EnvironmentVariable);
+
+const connectionOptions = dbSettings.POSTGRES_URL
+  ? {
+      url: dbSettings.POSTGRES_URL,
+    }
+  : {
+      host: dbSettings.POSTGRES_HOST,
+      port: dbSettings.POSTGRES_PORT,
+      username: dbSettings.POSTGRES_USER,
+      password: dbSettings.POSTGRES_PASSWORD,
+    };
+
 const dataSource = new DataSource({
   type: 'postgres',
-  host: dbSettings.POSTGRES_HOST,
-  port: dbSettings.POSTGRES_PORT,
-  username: dbSettings.POSTGRES_USER,
-  password: dbSettings.POSTGRES_PASSWORD,
+  ...connectionOptions,
   database: dbSettings.POSTGRES_DB,
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
